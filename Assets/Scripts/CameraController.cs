2fa9b959745b57b9fbf238b2cf3418c4051b1939
EthanHelm2023 <824851@mystma.org>
2021-01-14 00:01:28 -0600

    .

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player (3)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(player.transform.position.x, 0, -10);
+
+
+
+    }
+}
--- Assets/Scripts/PlayerController3.cs
+++ Assets/Scripts/PlayerController3.cs
@@ -6,12 +6,18 @@ public class PlayerController3 : MonoBehaviour
 {
     public float jumpPower;
 
+    public float moveSpeed;
+
     Rigidbody2D rb;
+
+    SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+
+        sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,6 +26,28 @@ public class PlayerController3 : MonoBehaviour
        if (Input.GetKeyDown(KeyCode.Space)) 
         {
             rb.AddForce(transform.up * jumpPower);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector3(moveSpeed, rb.velocity.y, 0);
+            sr.flipX = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector3(-moveSpeed, rb.velocity.y, 0);
+            sr.flipX = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector3(0, 0, 0);
         }
 
     }
